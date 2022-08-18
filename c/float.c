ca483a35f4c76602607c8dac199c6fef1a9d9651
xjd <lynndon@gmail.com>
2022-08-18 10:48:37 +0800

    Update

--- c/float.c
+++ c/float.c
@@ -8,28 +8,14 @@ double __attribute__((noinline)) op(int op_type, double a, double b) {
     switch (op_type) {
         case 0:
             return a + b;
-        case 1:
-            return a - b;
-        case 2:
-            return a * b;
-        case 4:
-            return a / b;
         default:
-            return (a + b) * (a / b - b);
+            return a + b + 1.0;
     }
 }
 
 int main() {
-    double a = 100.1;
-    double b = 200.2;
-    double c = 300.3;
-    double d = 400.4;
-    double r = (c + d * b - 12.3456) / a;
-
-    int res = (int)r;
-    printf("%d\n", res);
-    r = op(res, 100.1, 200.2);
-    res = (int)r;
+    double res = op(0, 100.1, 200.2);
+    int r = (int)res;
     printf("%d\n", r);
     return 0;
 }
