f3a04ca7eed013d8705a8b2ff1862ae38f371658
xjd <lynndon@gmail.com>
2022-08-18 10:36:11 +0800

    Update

--- c/float.c
+++ c/float.c
@@ -4,6 +4,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+double __attribute__((noinline)) op(int op_type, double a, double b) {
+    switch (op_type) {
+        case 0:
+            return a + b;
+        case 1:
+            return a - b;
+        case 2:
+            return a * b;
+        case 4:
+            return a / b;
+        default:
+            return (a + b) * (a / b - b);
+    }
+}
+
 int main() {
     double a = 100.1;
     double b = 200.2;
@@ -13,5 +28,8 @@ int main() {
 
     int res = (int)r;
     printf("%d\n", res);
+    r = op(res, 100.1, 200.2);
+    res = (int)r;
+    printf("%d\n", r);
     return 0;
 }
